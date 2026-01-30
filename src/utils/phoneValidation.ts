b4f81c52c91fc2bf84a59bbe42d4a571beafd3d9
vatsal.goti-at-915680928116 <ZmE51ivo1GPV8lGHTsytxGeB6yQCSM04pQ/dtkZr9yE=>
2026-01-30 22:15:35 +0530

    new check

--- src/utils/phoneValidation.ts
+++ src/utils/phoneValidation.ts
@@ -3,6 +3,7 @@ import { PhoneNumberUtil } from "google-libphonenumber";
 export const isPhoneValid = (phone: string): boolean => {
   try {
     const phoneUtil = PhoneNumberUtil.getInstance();
+    // checking validity for US region; modify as needed
     return phoneUtil.isValidNumber(phoneUtil.parseAndKeepRawInput(phone, 'US'));
   } catch {
     return false;
